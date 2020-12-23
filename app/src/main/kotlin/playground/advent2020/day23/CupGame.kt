c8b2a1dc3aa919948f3d2590ef3f4cbad9fb5460
Jorrit Wiersma <jorrit.wiersma@keylane.com>
2020-12-23 21:42:24 +0100

    Add solution from reddit for AoC 2020 day 23.2
    Note: based on the solution posted by clumsveed on reddit.

--- app/src/main/kotlin/playground/advent2020/day23/CupGame.kt
+++ app/src/main/kotlin/playground/advent2020/day23/CupGame.kt
@@ -7,33 +7,86 @@ class CupGame(val l: Logger) {
         val startingCups = input.map { char -> char.toString().toInt() }.toList()
         l.debug("Starting cups: $startingCups")
 
-        var cups = startingCups
-        var currentCupIndex = 0
+        val nextCups = runGame(startingCups, 100, startingCups.size)
 
-        repeat (100) {
-            val currentCupValue = cups[currentCupIndex]
+        l.info("Cup neighbours at end: $nextCups")
 
-            val cupsToMove = cupsToRightOf(currentCupIndex, cups)
-            l.debug("Cups to move: $cupsToMove")
-
-            val cupsWithout = cups.minus(cupsToMove)
-            val destinationCup = cupsWithout.filter { cup -> cup < currentCupValue }.max() ?: (cupsWithout.max())
-            val destinationIndex = cupsWithout.indexOf(destinationCup) + 1 // Plus one because we place next to destination cup
-
-            cups = cupsWithout.take(destinationIndex).plus(cupsToMove).plus(cupsWithout.drop(destinationIndex))
-            currentCupIndex = (cups.indexOf(currentCupValue) + 1) % cups.size
-            l.debug("New cups: $cups, current cup: ${cups[currentCupIndex]}")
+        // Construct the string that we have to return with the cups behind cup 1.
+        var cup = 1
+        var cupsAfter1 = ""
+        repeat(startingCups.size - 1) {
+            cup = nextCups[cup]
+            cupsAfter1 += cup
         }
 
-        l.info("Cups at end: $cups")
-
-        val indexOf1 = cups.indexOf(1)
-        return cups.drop(indexOf1 + 1).joinToString("") + cups.take(indexOf1).joinToString("")
+        return cupsAfter1
     }
 
-    private fun cupsToRightOf(currentCupIndex: Int, cups: List<Int>): List<Int> = List(3) { i ->
-        cups[(currentCupIndex + 1 + i) % cups.size]
+    fun playBigGame(input: String): Long {
+        val startingCups = input.map { char -> char.toString().toInt() }.toList()
+        l.debug("Starting cups: $startingCups")
+
+        val nextCups = runGame(startingCups, 10_000_000, 1_000_000)
+
+        l.info("Two cups next to 1: ${nextCups[1]} and ${nextCups[nextCups[1]]}")
+        return nextCups[1].toLong() * nextCups[nextCups[1]]
     }
 
+    private fun runGame(startingCups: List<Int>, iterations: Int, listSize: Int): List<Int> {
+        // Note: solution based on the solution posted by clumsveed on reddit.
+
+        // Create a list that contains the next cup for each cup, starting with ascending numbers.
+        val nextCups = MutableList(listSize + 1) { i ->
+            when (i) {
+                0 -> 0        // 0 is not used
+                listSize -> 1 // the last item points back to the start of the list
+                else -> i + 1 // the other items point to the next one
+            }
+        }
+
+        // Update the list with starting configuration.
+        startingCups.forEachIndexed { i, cup ->
+            when (i) {
+                startingCups.size - 1 -> nextCups[cup] = startingCups.size + 1 // The cup to the right of the last starting cup
+                else -> nextCups[cup] = startingCups[i + 1]
+            }
+        }
+
+        // Make sure the correct cup points back to the first of the starting cups.
+        if (startingCups.size == listSize) {
+            nextCups[startingCups.last()] = startingCups[0]
+        } else {
+            nextCups[listSize] = startingCups[0] // The cup all the way at the end will point to the first starting cup.
+        }
+
+        l.debug("Starting nextCups: $nextCups")
+
+        var currentCup = startingCups[0]
+
+        repeat(iterations) {
+            val cupToMove1 = nextCups[currentCup]
+            val cupToMove2 = nextCups[cupToMove1]
+            val cupToMove3 = nextCups[cupToMove2]
+            l.debug("Cups to move: $cupToMove1, $cupToMove2, $cupToMove3")
+
+            var destinationCup = currentCup - 1
+            if (destinationCup < 1) destinationCup += listSize
+            while (destinationCup == cupToMove1 || destinationCup == cupToMove2 || destinationCup == cupToMove3) {
+                destinationCup--
+                if (destinationCup < 1) destinationCup += listSize
+            }
+
+            nextCups[currentCup] = nextCups[cupToMove3]
+            val oldNextCupForDestination = nextCups[destinationCup]
+            nextCups[destinationCup] = cupToMove1
+            nextCups[cupToMove3] = oldNextCupForDestination
+
+            currentCup = nextCups[currentCup]
+
+            //l.debug("New nextCups: $nextCups")
+        }
+
+        return nextCups
+    }
 
 }
--- app/src/test/kotlin/playground/advent2020/day23/CupGameTest.kt
+++ app/src/test/kotlin/playground/advent2020/day23/CupGameTest.kt
@@ -2,15 +2,23 @@ package playground.advent2020.day23
 
 import playground.utilities.LogLevel
 import playground.utilities.Logger
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CupGameTest {
-    val l = Logger(LogLevel.DEBUG)
 
     @Test
     fun `should play 100 rounds of the cup game`() {
-        val result = CupGame(l).play100Rounds("389125467")
+        val result = CupGame(Logger(LogLevel.DEBUG)).play100Rounds("389125467")
         assertEquals("67384529", result)
+    }
+
+
+    @Test
+    @Ignore("Slow test, only run manually")
+    fun `should play the big cup game`() {
+        val result = CupGame(Logger(LogLevel.INFO)).playBigGame("389125467")
+        assertEquals(149245887792, result)
     }
 }
